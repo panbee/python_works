e4a9870ed38460732ac12804b0c77d5b33f76211
任盼盼 <panbee@163.com>
2022-06-28 14:41:59 +0800

    0628

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,9 @@
-import sys
-
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -13,21 +14,16 @@ def run_game():
         )
     pygame.display.set_caption("Alien Invasion")
 
-    # 设置背景色
-    bg_color = (230, 230, 230)
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 开始游戏的主循环
     while True:
-
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # 每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
