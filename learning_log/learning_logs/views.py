f212a7f28b9600668681c59eb26a40714e94d366
任盼盼 <panbee@163.com>
2022-07-25 15:42:26 +0800

    0725

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Topic
 
 # Create your views here.
+def index(request):
+    """学习笔记的主页"""
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('-date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
